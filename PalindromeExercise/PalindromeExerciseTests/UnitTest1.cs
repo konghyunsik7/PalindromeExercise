b4ffd2e64873080562460ff576fb1cbc6c086e49
Hyun Kong <konghyunsik7@gmail.com>
2022-07-25 16:49:09 -0400

    updated

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,31 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("dad", true)]
+        [InlineData("Hannah", true)]
+        [InlineData(" ", true)]
+        [InlineData("never odd or even ", true)]
+
+        public void PalindromTestTakesInString_OutputsBoolIfPalindrome(string word, bool expected)
         {
+            //Arrange
+
+            var wordSmith = new WordSmith();
+
+            //Act
+
+            var answer = wordSmith.IsAPalindrome(word);
+
+            //Assert
+
+            Assert.Equal(expected,answer);
 
         }
     }
